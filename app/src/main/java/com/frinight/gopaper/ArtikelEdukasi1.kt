5c4e2b7d5b36af3b94ad80f94a41e204e398cd64
inipunyafaqih <fckihh.a@gmail.com>
2023-11-23 06:28:00 +0700

    add education

---
+++ app/src/main/java/com/frinight/gopaper/ArtikelEdukasi1.kt
@@ -0,0 +1,11 @@
+package com.frinight.gopaper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ArtikelEdukasi1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_artikel_edukasi1)
+    }
+}
