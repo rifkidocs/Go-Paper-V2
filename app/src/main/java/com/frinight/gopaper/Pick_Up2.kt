ad14f6e0102922e0f01bf8eb2928743b28f1eaf5
Muhammad Nofa Nurhidayah <muhammadnofa722@gmail.com>
2023-11-22 21:30:44 +0700

    add pickup

---
+++ app/src/main/java/com/frinight/gopaper/Pick_Up2.kt
@@ -0,0 +1,11 @@
+package com.frinight.gopaper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Pick_Up2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pick_up2)
+    }
+}
